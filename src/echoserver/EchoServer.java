c38575b44aab08a6344b8e44f19b6c4560512b87
Nic McPhee <mcphee@morris.umn.edu>
2013-10-28 00:57:14 -0500

    First draft of the finished server code

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,29 @@
 package echoserver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
+	public static final int PORT_NUMBER = 6013;
+
+	public static void main(String[] args) throws IOException {
+		EchoServer server = new EchoServer();
+		server.start();
+	}
+
+	private void start() throws IOException {
+		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+		while (true) {
+			Socket socket = serverSocket.accept();
+			InputStream inputStream = socket.getInputStream();
+			OutputStream outputStream = socket.getOutputStream();
+			int b;
+			while ((b = inputStream.read()) != -1) {
+				outputStream.write(b);
+			}
+		}
+	}
 }
