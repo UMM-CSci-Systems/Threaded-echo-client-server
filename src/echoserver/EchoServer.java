1fbdffd1fa010179d5eeef31b48a1ddf659aa0b2
Nic McPhee <mcphee@morris.umn.edu>
2013-10-29 20:43:36 -0500

    A pile of flailing to try to get the server to work, but so far to no avail.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -9,21 +9,27 @@ import java.net.Socket;
 public class EchoServer {
 	public static final int PORT_NUMBER = 6013;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
 	}
 
-	private void start() throws IOException {
+	private void start() throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
 		while (true) {
 			Socket socket = serverSocket.accept();
 			InputStream inputStream = socket.getInputStream();
 			OutputStream outputStream = socket.getOutputStream();
 			int b;
+			int numBytes = 0;
 			while ((b = inputStream.read()) != -1) {
 				outputStream.write(b);
+				++numBytes;
 			}
+			outputStream.flush();
+//            socket.shutdownOutput();
+            System.err.println("The server processed " + numBytes + " bytes");
+//            Thread.sleep(1000);
 		}
 	}
 }
